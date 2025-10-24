e9b744568208b05440866a0c8df45a6ba73e8333
AnirbanNath-dev <anirbaniitian@gmail.com>
2025-10-24 11:58:01 +0530

    Add constants pkg for profile status

---
+++ constants/alias.go
@@ -0,0 +1,9 @@
+package constants
+
+var ProfileAliases = map[string]string{
+	"music": "a2dp-sink",
+	"call":  "headset-head-unit",
+	"a2dp":  "a2dp-sink",
+	"hsp":   "headset-head-unit",
+	"hfp":   "headset-head-unit",
+}
---
+++ constants/enum.go
@@ -0,0 +1,9 @@
+package constants
+
+type ProfileStatus string
+
+const (
+	A2DP ProfileStatus = "a2dp-sink"
+	HSP ProfileStatus = "headset-head-unit"
+	HFP ProfileStatus = "headset-head-unit"
+)
